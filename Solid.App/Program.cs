b86402fc33e2306cd9a982d5db74ed1b05c7d4f5
Samet ALTUNDAG <smtaltundag63@gmail.com>
2024-02-11 22:47:00 +0300

    Single Responsibility ve Open-Closed prensipleri tamamlandı.

---
+++ Solid.App/Program.cs
@@ -0,0 +1,34 @@
+﻿//using Solid.App.OCPBad;
+//using Solid.App.OCPGood;
+using Solid.App.OCPGood2;
+
+/*
+ * 
+ * OCP Bad Way
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+Console.WriteLine($"Low Salary: { salaryCalculator.Calculate(1000, SalaryType.Low)}");
+Console.WriteLine($"Middle Salary: {salaryCalculator.Calculate(2000, SalaryType.Middle)}");
+Console.WriteLine($"High Salary: {salaryCalculator.Calculate(3000, SalaryType.High)}");
+*/
+
+/*
+// OCP Good Way
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+Console.WriteLine($"Low Salary: {salaryCalculator.Calculate(1000, new LowSalaryCalculate())}");
+Console.WriteLine($"Middle Salary: {salaryCalculator.Calculate(1000, new MiddleSalaryCalculate())}");
+Console.WriteLine($"High Salary: {salaryCalculator.Calculate(1000, new HighSalaryCalculate())}");
+Console.WriteLine($"Manager Salary: {salaryCalculator.Calculate(1000, new ManagerSalaryCalculate())}");
+*/
+
+// OCP Good Way 2
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+Console.WriteLine($"Low Salary: {salaryCalculator.Calculate(1000, new LowSalaryCalculate().Calculate)}");
+Console.WriteLine($"Middle Salary: {salaryCalculator.Calculate(1000, new MiddleSalaryCalculate().Calculate)}");
+Console.WriteLine($"High Salary: {salaryCalculator.Calculate(1000, new HighSalaryCalculate().Calculate)}");
+Console.WriteLine($"Manager Salary: {salaryCalculator.Calculate(1000, new ManagerSalaryCalculate().Calculate)}");
+
+Console.WriteLine($"Custom Salary: {salaryCalculator.Calculate(1000, x =>
+{
+    return x * 10;
+} )}");
---
+++ Solid.App/SRPBad.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.SRP.Bad
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private static List<Product> ProductsList = new List<Product>();
+        public List<Product> GetProducts => ProductsList;
+
+        public Product()
+        {
+            ProductsList = new()
+            {
+                new () {Id = 1,Name = "Product 1"},
+                new () {Id = 2,Name = "Product 2"},
+                new () {Id = 3,Name = "Product 3"},
+                new () {Id = 4,Name = "Product 4"}
+            };
+        }
+
+
+        // Alttaki kodlar SOLID prensiplerinin dışındadır
+
+        public void SaveOrUpdate(Product product)
+        {
+            var hasProduct = ProductsList.Any(p => p.Id == product.Id);
+
+            if(!hasProduct)
+            {
+                ProductsList.Add(product);
+            }
+            else
+            {
+                var index = ProductsList.FindIndex(x=> x.Id == product.Id);
+                ProductsList[index] = product;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var hasProduct = ProductsList.Find(x=>x.Id == id);
+
+            if (hasProduct == null)
+            {
+                throw new Exception("Ürün bulunamadı.");
+            }
+
+            ProductsList.Remove(hasProduct);
+        }
+
+        public void WriteToConsole()
+        {
+            ProductsList.ForEach(x =>
+            {
+                Console.WriteLine($"{x.Id} - {x.Name}");
+            });
+        }
+    }
+}
